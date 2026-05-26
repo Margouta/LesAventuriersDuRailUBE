5755237bc37a77ef1e3d1f3c7b759010a3afebbe
Margouta <56451786+Margouta@users.noreply.github.com>
2026-05-26 16:40:53 +0200

    Add files via upload

---
+++ Joueur.cpp
@@ -0,0 +1,24 @@
+#include "Joueur.h"
+#include "Route.h"
+
+Joueur::Joueur(const std::string& nom)
+	: nom(nom)
+{
+
+}
+void Joueur::piocherTransport()
+{
+
+}
+void Joueur::prendreRoute(Route*)
+{
+
+}
+void Joueur::piocherDestination()
+{
+
+}
+void Joueur::verifierDrapeau()
+{
+
+}
---
+++ Joueur.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <map>
+#include "Couleurs.h"
+class Joueur
+{
+private:
+	const std::string nom;
+	unsigned int nombre_autobus;
+	std::map<Couleurs, int> cartesTransport;
+	std::vector<CarteDestination*> cartesDestination;
+	int points;
+	std::vector<Route*> routesPossedees;
+public:
+	Joueur(const std::string& nom);
+	void piocherTransport();
+	void prendreRoute(Route*);
+	void piocherDestination();
+	void verifierDrapeau();
+};
+
